a2c4981039107f8b6a6eeba8b9b6be58ace5a9f9
Chengbin <chengbin.feng@unsw.edu.au>
2021-02-05 16:48:02 +1100

    add files

--- pilot/models.py
+++ pilot/models.py
@@ -32,10 +32,10 @@ class Player(BasePlayer):
     answer_check = models.StringField()
     round_number  = models.IntegerField(initial=60)
 
-    screen3_q1 = models.StringField(label = "1. Did you find the task easy or difficult?")
-    screen3_q2 = models.StringField(label = "2. Did you find the task boring or interesting?")
+    screen3_q1 = models.StringField(label = "1. Did you find the task easy or difficult?",  choices=["Very Easy", "Easy", "Somewhat Easy", "Neutral", "Somewhat Difficult", "Difficult", "Very difficult"], widget=widgets.RadioSelect )
+    screen3_q2 = models.StringField(label = "2. Did you find the task boring or interesting?",  choices = ["Very Boring", "Boring", "Somewhat Boring", "Neutral", "Somewhat Interesting", "Interesting", "Very Interesting"], widget = widgets.RadioSelect  )
     age = models.StringField(label="3. What is your age?")
-    gender = models.StringField(label = "4. What is your gender?")
+    gender = models.StringField(label = "4. What is your gender?", choices=["Male", "Female", "Other/Prefer not to say"], widget=widgets.RadioSelect )
 
     def get_adjusted_num_questions_left(self):
         return self.round_number
